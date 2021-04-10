839da6b31400bc7ca6688f08f431c1703ba879a8
Влад Шахно <v.shahno@outlook.com>
2021-04-10 17:35:31 +0300

    All methods now works with JDBCTemplate

--- src/main/java/ua/edu/nupp/config/SpringConfig.java
+++ src/main/java/ua/edu/nupp/config/SpringConfig.java
@@ -1,10 +1,14 @@
 package ua.edu.nupp.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -48,5 +52,22 @@ public class SpringConfig implements WebMvcConfigurer {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
+    }
+    
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/DiplomaDataBase");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("1234");
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
     }
 }
--- src/main/java/ua/edu/nupp/dao/LeadDAO.java
+++ src/main/java/ua/edu/nupp/dao/LeadDAO.java
@@ -1,148 +1,44 @@
 package ua.edu.nupp.dao;
 
-import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ua.edu.nupp.models.*;
-import java.sql.*;
 
 @Component
 public class LeadDAO {
-    private static int LEADS_COUNT;
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/DiplomaDataBase";
-    private static final String USERNAME = "postgres";
-    private static final String PASSWORD = "1234";
+    private final JdbcTemplate jdbcTemplate;
 
-    private static Connection connection;
-
-    static {
-        try {
-            Class.forName("org.postgresql.Driver");
-        } 
-        catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        } 
-        catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+    @Autowired
+    public LeadDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
-
-
 
     public List<Lead> index() {
-        List<Lead> leads = new ArrayList<>();
-
-        try {
-            Statement statement = connection.createStatement();
-            String SQL = "SELECT * FROM lead";
-            ResultSet resultSet = statement.executeQuery(SQL);
-
-            while(resultSet.next()) {
-                Lead lead = new Lead();
-
-                lead.setId(resultSet.getInt("id"));
-                lead.setFirstName(resultSet.getString("firstname"));
-                lead.setLastName(resultSet.getString("lastname"));
-                lead.setTitle(resultSet.getString("title"));
-                lead.setEmail(resultSet.getString("email"));
-                lead.setProof(resultSet.getString("proof"));
-                leads.add(lead);
-            }
-
-        } 
-        catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
-        return leads;
+        return jdbcTemplate.query("SELECT * FROM lead", new BeanPropertyRowMapper<>(Lead.class));
     }
 
+    @SuppressWarnings("deprecation")
     public Lead show(int id) {
-        Lead lead = null;
-
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("SELECT * FROM lead WHERE id=?");
-
-            preparedStatement.setInt(1, id);
-
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            resultSet.next();
-
-            lead = new Lead();
-
-            lead.setId(resultSet.getInt("id"));
-            lead.setFirstName(resultSet.getString("firstname"));
-            lead.setLastName(resultSet.getString("lastname"));
-            lead.setTitle(resultSet.getString("title"));
-            lead.setEmail(resultSet.getString("email"));
-            lead.setProof(resultSet.getString("proof"));
-        } 
-        catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
-        return lead;
+        return jdbcTemplate.query("SELECT * FROM lead WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Lead.class))
+                .stream().findAny().orElse(null);
     }
 
     public void save(Lead lead) {
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("INSERT INTO lead VALUES(1, ?, ?, ?,?,?)");
-
-            preparedStatement.setString(1, lead.getFirstName());
-            preparedStatement.setString(2, lead.getLastName());
-            preparedStatement.setString(3, lead.getTitle());
-            preparedStatement.setString(4, lead.getEmail());
-            preparedStatement.setString(5, lead.getProof());
-
-            preparedStatement.executeUpdate();
-        } 
-        catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        jdbcTemplate.update("INSERT INTO lead VALUES(1, ?, ?, ?, ?, ?)", lead.getFirstName(), lead.getLastName(), 
+                lead.getTitle(), lead.getEmail(), lead.getProof());
     }
 
     public void update(int id, Lead updatedLead) {
-        try {
-            PreparedStatement preparedStatement =
-                    connection.prepareStatement("UPDATE lead SET firstname=?, lastname=?, "
-                            + "title=?, email=?, proof=? WHERE id=?");
-
-            preparedStatement.setString(1, updatedLead.getFirstName());
-            preparedStatement.setString(2, updatedLead.getLastName());
-            preparedStatement.setString(3, updatedLead.getTitle());
-            preparedStatement.setString(4, updatedLead.getEmail());
-            preparedStatement.setString(5, updatedLead.getProof());
-            preparedStatement.setInt(6, id);
-
-            preparedStatement.executeUpdate();
-        } 
-        catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        jdbcTemplate.update("UPDATE lead SET firstname=?, lastname=?, title=?, email=?, proof=? WHERE id=?", 
+                updatedLead.getFirstName(), updatedLead.getLastName(), updatedLead.getTitle(), 
+                updatedLead.getEmail(), updatedLead.getProof(), id);
     }
 
     public void delete(int id) {
-        PreparedStatement preparedStatement = null;
-
-        try {
-            preparedStatement = connection.prepareStatement("DELETE FROM lead WHERE id=?");
-
-            preparedStatement.setInt(1, id);
-
-            preparedStatement.executeUpdate();
-        } 
-        catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
-
+        jdbcTemplate.update("DELETE FROM lead WHERE id=?", id);
     }
 }
